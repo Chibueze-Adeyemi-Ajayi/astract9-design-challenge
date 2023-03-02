6248f6549bfff56ccf3a755ca509249db20c2d39
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-02 19:25:29 +0100

    Frontend engineering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/app-bar';
 
 function App() {
   return (
     <section className='w-full h-full p-6 flex flex-col space-y-4 bg-gray-900'>
-
+      {/* application app bar */}
+      <AppBar />
     </section>
   );
 }
---
+++ src/components/app-bar.js
@@ -0,0 +1,7 @@
+const AppBar = () => {
+    return <section className="w-full h-20 bg-inherit flex flex-row space-x-6">
+        
+    </section>
+}
+
+export default AppBar;
