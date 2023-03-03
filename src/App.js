fc56f336c20bc0f12428d99845d9c937b728efe0
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-03 19:56:10 +0100

    frontend engineering

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import OtherElectionResults from './components/other-election-results';
 import Stateresults from './components/state-results';
 import Footer from './components/footer';
 
-// this project is a fro
+// this project is a fron
 
 function App() {
   return (
