ff3b50e1e2b544f2700a43817f28daa7c3edba6c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-08 06:51:43 +0100

    frontend engineering

--- src/assets/js/http-connector.js
+++ src/assets/js/http-connector.js
@@ -4,15 +4,21 @@
 
 import $ from "jquery";
 
-var settings = {
-    "url": "{{staging}}/elections/candidate-total-votes?type=president",
-    "method": "GET",
-    "timeout": 0,
-    "headers": {
-      "Accept": "application/json"
-    },
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+  function FetchHTTPData (param, callback) {
+
+    var settings = {
+        "url": `{{staging}}/elections/candidate-total-votes?type=${param}`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+        "Accept": "application/json"
+        },
+    };
+    
+    $.ajax(settings).done(function (response) {
+        callback(response);
+    });
+
+  }
+
+  module.exports = {FetchHTTPData}
