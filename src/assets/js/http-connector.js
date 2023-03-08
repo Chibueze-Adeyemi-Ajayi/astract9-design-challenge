b98d399298ff8cbae525d8cad2120e43086ef55f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-08 06:49:26 +0100

    frontend engineering

--- src/assets/js/http-connector.js
+++ src/assets/js/http-connector.js
@@ -1,3 +1,16 @@
 /*
 * This code snippet enables http communication with backend 
 */
+
+var settings = {
+    "url": "{{staging}}/elections/candidate-total-votes?type=president",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Accept": "application/json"
+    },
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
