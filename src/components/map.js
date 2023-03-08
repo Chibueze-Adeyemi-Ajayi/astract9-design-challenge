b680bb33598e0eb4b7aeda4e054f3e9249651bd5
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-09 00:09:45 +0100

    frontend engineering

--- src/components/map.js
+++ src/components/map.js
@@ -12,7 +12,7 @@ var css = `<style>
       .btn_simplemaps:hover{text-decoration: underline;}
 </style>`;
 
-// This code section emb
+// This code section embe
 function jsRender (block) { localStorage.setItem('state-result', JSON.stringify(block));
 console.log(JSON.parse(localStorage.getItem('state-result')));
     setTimeout(() => {
