aea3b7570c443e409ab20611193d28c459172289
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-10 17:02:42 +0100

    Frontend Engineering

--- src/components/map.js
+++ src/components/map.js
@@ -95,7 +95,7 @@ console.log(JSON.parse(localStorage.getItem('state-result')));
 function loadStateResults () {
     FetchHTTPData("state_result", response => jsRender(response));
 }
-// display the generated (Scal) SVG map
+// display the generated (Scala) SVG map
 const Map = () => { 
     useEffect(loadStateResults);
     return <>
